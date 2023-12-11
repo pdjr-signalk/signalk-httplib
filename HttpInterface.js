651627b47744033809292e0ff42138d946c4d5af
Paul Reeve <preeve@pdjr.eu>
2023-12-11 21:57:25 +0100

    Update HttpInterface.js

--- HttpInterface.js
+++ HttpInterface.js
@@ -39,7 +39,7 @@ module.exports = class HttpInterface {
       for (const name of Object.keys(nets)) {
         for (const net of nets[name]) {
           const familyV4Value = typeof net.family === 'string' ? 'IPv4' : 4
-          if ((net.family === familyV4Value) && (!net.internal) && (!net.address.startsWith('168.254.'))) this.serverAddress = net.address;
+          if ((net.family === familyV4Value) && (!net.internal) && (!net.address.startsWith('169.254.'))) this.serverAddress = net.address;
         }
       }
     }
